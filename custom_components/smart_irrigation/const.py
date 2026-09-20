555be76ab9c4c7198b2f1294764a115c6e91718f
altmenorg <tonio@ubuntu.com>
2026-09-21 00:29:04 +0200

    release: v2026.9.1-beta1

--- custom_components/smart_irrigation/const.py
+++ custom_components/smart_irrigation/const.py
@@ -1,6 +1,6 @@
 """Store constants."""
 
-VERSION = "v2026.9.0"
+VERSION = "v2026.9.1-beta1"
 NAME = "Smart Irrigation"
 MANUFACTURER = "@altmenorg"
 
--- custom_components/smart_irrigation/frontend/dist/smart-irrigation.js
+++ custom_components/smart_irrigation/frontend/dist/smart-irrigation.js
@@ -4118,7 +4118,7 @@ a.version="2.31.0",i(ii),a.fn=fs,a.min=oi,a.max=li,a.now=di,a.utc=m,a.unix=bs,a.
             .narrow=${this.narrow}
           ></ha-menu-button>
           <div class="main-title">${dr("title",this.hass.language)}</div>
-          <div class="version">${"v2026.9.0"}</div>
+          <div class="version">${"v2026.9.1-beta1"}</div>
         </div>
 
         ${t&&a?q`
